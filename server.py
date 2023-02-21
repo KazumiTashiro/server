936159c497208ce25affa69504a3d85437df8157
KazumiTashiro <al20022@shibaura-it.ac.jp>
2023-02-21 16:57:21 +0900

    サーバプログラムの修正(2023/2/21)

--- server.py
+++ server.py
@@ -1,3 +1,17 @@
+"""
+server.pyの仕様について
+・ルートパス(/)にGETメソッドでアクセスした時にindex.htmlを返す
+・/luxパスにGETメソッドでアクセスすると、sensor_data.csvの内容を返す
+・/luxパスにPOSTメソッドで、時間と照明値のパラメータを付与してアクセスするとsensor_data.csvに内容を上書きで書き込む
+
+GETメソッド・・・HTTP通信でWebブラウザ等のクライアントからWebサーバへ送られる、HTTPリクエストの一種である。
+webサーバから情報を取り出すために使用される。(HTTPリエスト歯、リクエスト行、ヘッダ、メッセージボディの３つの部分で構成される。)
+
+POSTメソッド・・・HTTP通信でWebブラウザ等のクライアントからWebサーバへと送られる、HTTPリクエストの一種です。 基本的に、
+ebサーバに情報を送り出す（POST）するために使用されます。 HTTPリクエストは、リクエスト行、ヘッダ、メッセージ ボディの3つの部分で構成されます。
+"""
+
+
 from flask import Flask,request,render_template
 app = Flask(__name__)
 file_path ="./sensor_data.csv"
@@ -6,6 +20,7 @@ port_num=20022
 @app.route('/',methods=['GET'])
 def get_html():
     return render_template('./index.html')
+
 @app.route('/lux',methods=['POST'])
 def update_lux():
     time = request.form["time"]
@@ -18,7 +33,7 @@ def update_lux():
         print(e)
         return "failed to write"
     finally:
-        f.close()
+        f.close()                      
 @app.route('/lux',methods=['GET'])
 
 def get_lux():
